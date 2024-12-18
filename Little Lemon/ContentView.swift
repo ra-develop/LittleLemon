313866396a82dc922570d2aabeccdeaf754d5398
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-12-18 20:13:14 +0100

    Initial Commit

---
+++ Little Lemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by R.A. on 18/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by R.A. on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
