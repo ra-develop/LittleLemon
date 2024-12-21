624849d407ce241b4aee4062f3867df2a7128e59
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-12-21 16:30:07 +0100

    1. Updated SignIn / Loggin logyc; 2.Added UserProfile view

---
+++ Little Lemon/Views/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  Little Lemon
+//
+//  Created by R.A. on 21/12/24.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
