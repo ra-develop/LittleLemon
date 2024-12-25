a35f37b91c3d23aee1593ecd691c96b34a816922
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-12-25 16:59:33 +0100

    Updating project: 1. Setup assets for collors, logos and images; 2. Setup button styles; 3. Modifed navigation method from tab to stack 3. Modified disign, layout, logic of the UserProfile view; 4. Modified logic of Onboarding view

---
+++ Little Lemon/CoreViews/ButtonStyles.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonStyles.swift
+//  Little Lemon
+//
+//  Created by R.A. on 24/12/24.
+//
+
+import SwiftUI
+
+struct ButtonStyles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonStyles()
+}
